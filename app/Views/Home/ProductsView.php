43d0cffbbac257ab2275a287807673f37bd7f97c
hrodriguezg5 <hrodriguezg5@miumg.edu.gt>
2024-05-27 14:08:44 -0600

    Detallando Productos

--- app/Views/Home/ProductsView.php
+++ app/Views/Home/ProductsView.php
@@ -9,9 +9,23 @@
                     <img src="<?= URL_ROUTE; ?>/img/S06A.jpg">
                 </div>
 
-                <h2>Hamburguesa de pollo</h2>
-                <h3>Comida Sabrosa</h3>
-                <span>$14.00</span>
+                <h1>GPS 3G</h1>
+				<ul>
+					<li>Rastreo en tiempo Real</li>
+					<li>Apagado remoto</li>
+					<li>Monitoreo de velocidad </li>
+					<li>Monitoreo de rutas</li>
+					<li>Alarma ACC on/off</li>
+					<li>Alarma de exceso de velocidad</li>
+					<li>Alarma de batería baja</li>
+					<li>Micrófono espía</li>
+					<li>Geo cercas</li>
+					<li>Botón de pánico</li>
+					<li>Batería de Respaldo Interna (5 Horas)</li>
+					<li>Control de mantenimiento del Vehículo<br> por kilometraje</li>
+					<li>Voltaje de Batería del Vehículo</li>
+				</ul>
+                <span>Q650.00</span>
             </div>
 
             <div class="product_content">
@@ -19,9 +33,23 @@
                     <img src="<?= URL_ROUTE; ?>/img/s5l.jpg">
                 </div>
 
-                <h2>Hamburguesa Especial</h2>
-                <h3>Comida Sabrosa</h3>
-                <span>$11.00</span>
+                <h1>GPS 4G</h1>
+                <ul>
+					<li>Rastreo en tiempo Real</li>
+					<li>Apagado remoto</li>
+					<li>Monitoreo de velocidad </li>
+					<li>Monitoreo de rutas</li>
+					<li>Alarma ACC on/off</li>
+					<li>Alarma de exceso de velocidad</li>
+					<li>Alarma de batería baja</li>
+					<li>Micrófono espía</li>
+					<li>Geo cercas</li>
+					<li>Botón de pánico</li>
+					<li>Batería de Respaldo Interna (5 Horas)</li>
+					<li>Control de mantenimiento del Vehículo<br> por kilometraje</li>
+					<li>Voltaje de Batería del Vehículo</li>
+				</ul>
+                <span>Q700.00</span>
             </div>
 
             <div class="product_content">
@@ -29,9 +57,23 @@
                     <img src="<?= URL_ROUTE; ?>/img/r12l.jpg">
                 </div>
 
-                <h2>Pollo Frito</h2>
-                <h3>Comida Sabrosa</h3>
-                <span>$18.00</span>
+                <h1>GPS 4G Moto</h1>
+                <ul>
+					<li>Rastreo en tiempo Real</li>
+					<li>Apagado remoto</li>
+					<li>Monitoreo de velocidad </li>
+					<li>Monitoreo de rutas</li>
+					<li>Alarma ACC on/off</li>
+					<li>Alarma de exceso de velocidad</li>
+					<li>Alarma de batería baja</li>
+					<li>Micrófono espía</li>
+					<li>Geo cercas</li>
+					<li>Botón de pánico</li>
+					<li>Batería de Respaldo Interna (5 Horas)</li>
+					<li>Control de mantenimiento del Vehículo<br> por kilometraje</li>
+					<li>Voltaje de Batería del Vehículo</li>
+				</ul>
+                <span>Q700.00</span>
             </div>
         </div>
     </section>
