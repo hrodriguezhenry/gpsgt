bc0c8381350881a4873063f23ad952d17a9d6f52
hrodriguezg5 <hrodriguezg5@miumg.edu.gt>
2024-05-28 14:11:26 -0600

    Reestructurando sección de citas

--- app/Views/Home/BookingView.php
+++ app/Views/Home/BookingView.php
@@ -1,8 +1,36 @@
-<br>
-<br>
-<br>
-<br>
-<section id="booking" class="book_section layout_padding">
+<section class="booking" id="booking">
+        <div class="booking_title">
+            <h1>Agendar Cita</h1>
+        </div>
+
+        <div class="products_content">
+            <div class="product_content">
+                <div class="product_content_img">
+                    <img src="<?= URL_ROUTE; ?>/img/S06A.jpg">
+                </div>
+
+                <h1>GPS 3G</h1>
+				<ul>
+					<li>Rastreo en tiempo Real</li>
+					<li>Apagado remoto</li>
+					<li>Monitoreo de velocidad </li>
+					<li>Monitoreo de rutas</li>
+					<li>Alarma ACC on/off</li>
+					<li>Alarma de exceso de velocidad</li>
+					<li>Alarma de batería baja</li>
+					<li>Micrófono espía</li>
+					<li>Geo cercas</li>
+					<li>Botón de pánico</li>
+					<li>Batería de Respaldo Interna (5 Horas)</li>
+					<li>Control de mantenimiento del Vehículo<br> por kilometraje</li>
+					<li>Voltaje de Batería del Vehículo</li>
+				</ul>
+                <span>Q650.00</span>
+            </div>
+        </div>
+    </section>
+
+	<section id="booking1" class="book_section layout_padding">
 		<div class="container">
 		<div class="heading_container">
 			<br>
