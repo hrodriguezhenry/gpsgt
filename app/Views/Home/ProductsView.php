260dda84313b8c52ae88278ff84b91fded837245
hrodriguez <hrodriguezhenry@gmail.com>
2024-05-27 22:40:10 -0600

    Reestructurando sección de productos

--- app/Views/Home/ProductsView.php
+++ app/Views/Home/ProductsView.php
@@ -45,9 +45,11 @@
 					<li>Micrófono espía</li>
 					<li>Geo cercas</li>
 					<li>Botón de pánico</li>
-					<li>Batería de Respaldo Interna (5 Horas)</li>
+					<li>Batería de Respaldo Interna (6 Horas)</li>
 					<li>Control de mantenimiento del Vehículo<br> por kilometraje</li>
 					<li>Voltaje de Batería del Vehículo</li>
+					<li>Mayor Cobertura y velocidad<br> de respuesta</li>
+					<li>Mayor precision de rastreo</li>
 				</ul>
                 <span>Q700.00</span>
             </div>
@@ -57,7 +59,7 @@
                     <img src="<?= URL_ROUTE; ?>/img/r12l.jpg">
                 </div>
 
-                <h1>GPS 4G Moto</h1>
+                <h1>GPS 4G Motocicleta</h1>
                 <ul>
 					<li>Rastreo en tiempo Real</li>
 					<li>Apagado remoto</li>
@@ -65,107 +67,16 @@
 					<li>Monitoreo de rutas</li>
 					<li>Alarma ACC on/off</li>
 					<li>Alarma de exceso de velocidad</li>
-					<li>Alarma de batería baja</li>
-					<li>Micrófono espía</li>
+					<li>Alarma de batería baja y manipulacion</li>
 					<li>Geo cercas</li>
 					<li>Botón de pánico</li>
-					<li>Batería de Respaldo Interna (5 Horas)</li>
-					<li>Control de mantenimiento del Vehículo<br> por kilometraje</li>
+					<li>Batería de Respaldo Interna (4 Horas)</li>
+					<li>Voltaje de Batería de la Motocicleta</li>
 					<li>Voltaje de Batería del Vehículo</li>
+					<li>A prueba de Agua</li>
+					<li>Compactibilidad</li>
 				</ul>
                 <span>Q700.00</span>
             </div>
         </div>
     </section>
-
-	<section id="products" class="products">
-		<div class="product_container">
-			<div class="products_header">
-				<h2>Nuestros Productos</h2>
-			</div>
-
-			<div class="gps-list-content">
-				<div class="owl-carousel owl-theme" id="gps-list-carousel">
-					<div class="gps-list-item">
-						<div class="single-gps-list-item">
-							<div class="row">
-								<div class="col-md-7 col-sm-12">
-									<div class="gps-list-img">
-										<img src="<?= URL_ROUTE; ?>/img/S06A.jpg" alt="img"/>
-									</div><!--/.gps-list-img-->
-								</div>
-								<div class="col-md-5 col-sm-12">
-									<div class="gps-list-txt">
-										<center>
-										<h2><a href="#">GPS 3G </a></h2>
-										</center>
-										<p>
-											Placeholder para texto pendiente del gps.  
-										</p>
-										<p class="gps-list-para2">
-											Placeholder para texto pendiente del gps.  
-										</p>
-										<button class="welcome-btn gps-list-btn" onclick="window.location.href='#booking'">
-											Ir a Agendar
-										</button>
-									</div><!--/.gps-list-txt-->	
-								</div><!--/.col-->
-							</div><!--/.row-->
-						</div><!--/.single-gps-list-item-->
-					</div><!--/.gps-list-item-->
-					<div class="gps-list-item">
-						<div class="single-gps-list-item">
-							<div class="row">
-								<div class="col-md-7 col-sm-12">
-									<div class="gps-list-img">
-										<img src="<?= URL_ROUTE; ?>/img/s5l.jpg" alt="img"/>
-									</div><!--/.gps-list-img-->
-								</div>
-								<div class="col-md-5 col-sm-12">
-									<div class="gps-list-txt">
-										<center>
-										<h2><a href="#">GPS 4G </a></h2>
-										</center>
-										<p>
-											Placeholder para texto pendiente del gps.  
-										<p class="gps-list-para2">
-											Placeholder para texto pendiente del gps.  
-										<button class="welcome-btn gps-list-btn" onclick="window.location.href='#booking'">
-											Ir a Agendar											
-										</button>
-									</div><!--/.gps-list-txt-->	
-								</div><!--/.col-->
-							</div><!--/.row-->	
-						</div><!--/.single-gps-list-item-->
-					</div><!--/.gps-list-item-->
-					<div class="gps-list-item">
-						<div class="single-gps-list-item">
-							<div class="row">
-								<div class="col-md-7 col-sm-12">
-									<div class="gps-list-img">
-										<img src="<?= URL_ROUTE; ?>/img/r12l.jpg" alt="img"/>
-									</div><!--/.gps-list-img-->
-								</div>
-								<div class="col-md-5 col-sm-12">
-									<div class="gps-list-txt">
-										<center>
-										<h2><a href="#">GPS 4G Para Moto</a></h2>
-										</center>
-										<p>
-											Placeholder para texto pendiente del gps. 
-										</p> 
-										<p class="gps-list-para2">
-											Placeholder para texto pendiente del gps.											
-										</p>
-										<button class="welcome-btn gps-list-btn" onclick="window.location.href='#booking'">
-											Ir a Agendar
-										</button>
-									</div><!--/.gps-list-txt-->	
-								</div><!--/.col-->
-							</div><!--/.row-->
-						</div><!--/.single-gps-list-item-->
-					</div><!--/.gps-list-item-->
-				</div><!--/#gps-list-carousel-->
-			</div><!--/.gps-list-content-->
-		</div><!--/.container-->
-	</section><!--/.gps-list-->
